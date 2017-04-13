6e67dc866ade8e94fc224ffff70e659a4418b671
Nishtha <nishtha.arora26@gmail.com>
2017-04-12 22:40:26 -0500

    changing get request from http to https

--- app/components/todoFunStuff.js
+++ app/components/todoFunStuff.js
@@ -25,7 +25,7 @@ export default class ToDoFunStuff extends React.Component {
         let self = this;
         let phrase = event.target.value
         let newArr = [];
-        axios.get('http://api.giphy.com/v1/gifs/search?q=' + phrase + '&api_key=dc6zaTOxFJmzC')
+        axios.get('https://api.giphy.com/v1/gifs/search?q=' + phrase + '&api_key=dc6zaTOxFJmzC')
         .then((response) => {
             response.data.data.map((giphy,i) => {
                 newArr.push(giphy.images.fixed_height.url)
